3be3a3bb797d1159fb462e0923d05074f708341b
lolgithubbo <166530850+lolgithubbo@users.noreply.github.com>
2025-11-27 22:27:58 +0100

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Key Generator",
+  description: "Generiert Codes"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="de">
+      <body>{children}</body>
+    </html>
+  );
+}
